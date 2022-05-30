b3a607f483162ff27074325c17231f57c111457c
yonghan3 <yonghan3@iflytek.com>
2022-05-30 16:10:05 +0800

    read file

--- src/editor.rs
+++ src/editor.rs
@@ -1,6 +1,8 @@
 use crate::Document;
 use crate::Row;
 use crate::Terminal;
+use crate::document;
+use std::env;
 use termion::event::Key;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -126,10 +128,17 @@ impl Editor {
     }
 
     pub fn default() -> Self {
+        let args: Vec<String> = env::args().collect();
+        let document = if args.len() > 1 {
+            let file_name = &args[1];
+            Document::open(&file_name).unwrap_or_default()
+        } else {
+            Document::default()
+        };
         Self {
             should_quit: false,
             terminal: Terminal::default().expect("Failed to initialize terminal"),
-            document: Document::open(),
+            document,
             cursor_position: Position::default(),
         }
     }
